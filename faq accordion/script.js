d876fecaf76096fb2dd4034979be2006b3992534
Adrian Kurek <136102234+slodkiadrianek@users.noreply.github.com>
2024-01-03 15:48:42 +0100

    Add files via upload

--- faq accordion/script.js
+++ faq accordion/script.js
@@ -1,23 +1,34 @@
 'use strict '
-const buttons = document.querySelectorAll('button')
 const paragraphs = document.querySelectorAll('.paragraph')
 const header = document.querySelectorAll('.header')
-
-buttons.forEach((btn) => btn.addEventListener('click', function(e){
-    const num = btn.dataset.num;
+const img = document.querySelectorAll('.img')
+// OPEN/CLOSE
+header.forEach((touch) => touch.addEventListener('click', function(e){
+    const num = touch.dataset.num;
+    if(e.target.classList.contains('touch')){
     paragraphs.forEach(function(el,i){
-       if(i == num){
+       if(i === +num ){
         el.classList.toggle('hidden')
        }
     })
+}
 }))
+// zmiana obrazka
 
 header.forEach((touch) => touch.addEventListener('click', function(e){
-    const num = touch.dataset.num;
-    console.log(num);
+     const num = touch.dataset.num;
     paragraphs.forEach(function(el,i){
-       if(i === +num){
-        el.classList.toggle('hidden')
-       }
+                if( i=== +num && !el.classList.contains('hidden')){
+                    img.forEach(function(img,i){
+                        if(i===+num ){
+                            img.src ='images/icon-minus.svg'
+                    }})
+                }
+                if(i=== +num && el.classList.contains('hidden')){
+                    img.forEach(function(img,i){
+                        if(i===+num ){
+                            img.src ='images/icon-plus.svg'
+                    }})
+                }
     })
 }))
